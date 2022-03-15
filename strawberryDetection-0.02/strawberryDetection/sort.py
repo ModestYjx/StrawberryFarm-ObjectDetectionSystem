08804801afc027dea6bedbd9499fb73e5235c6af
ModestYjx <929604665@qq.com>
2022-03-15 20:06:56 +0800

    [feat] Rename images name  from "images_1" to "greenhouse_1" and update README.md from latest document

--- strawberryDetection-0.02/strawberryDetection/sort.py
+++
@@ -1,4 +0,0 @@
-dic = dict({0: 7, 1: 4, 2: 17, 3: 5, 4: 7, 5: 6, 6: 5, 7: 7, 8: 8, 9: 6, 10: 1, 11: 6})
-# a = [ v for v in sorted(dic.values())]
-a = sorted(dic.items(), key=lambda d: d[1])
-dic = dict(a)
